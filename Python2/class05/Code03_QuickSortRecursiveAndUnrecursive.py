fc8d9cf86c5e2cf9aa265ab338a5fc44b63221ba
yufeng.shi <yufeng.shi@kaiqigu.com>
2021-07-23 16:43:31 +0800

    快排迭代版

--- Python2/class05/Code03_QuickSortRecursiveAndUnrecursive.py
+++ Python2/class05/Code03_QuickSortRecursiveAndUnrecursive.py
@@ -3,7 +3,7 @@
 import random
 
 """
-荷兰国旗和快排
+快排3.0 迭代版本
 """
 
 
@@ -11,36 +11,14 @@ def swap(arr, i, j):
     arr[i], arr[j] = arr[j], arr[i]
 
 
-def partition(arr, L, R):
-    """
-    arr[L..R]上，以arr[R]位置的数做划分值
-    <= X 在左边
-    > X 的在右边
-    """
-    if L > R:
-        return -1
-    if L == R:
-        return L
-    lessEqual = L - 1
-    index = L
-    while index < R:
-        if arr[index] <= arr[R]:
-            lessEqual += 1
-            swap(arr, index, lessEqual)
-        index += 1
-    lessEqual += 1
-    swap(arr, lessEqual, R)
-    return lessEqual
-
-
-def netherlandsFlag(arr, L, R):
+def netherlandFlag(arr, L, R):
     if L > R:
         return [-1, -1]
     if L == R:
         return [L, R]
 
-    less = L - 1    # 小于区右边界
-    more = R        # 大于区左边界
+    less = L - 1
+    more = R
     index = L
     while index < more:
         if arr[index] < arr[R]:
@@ -56,34 +34,21 @@ def netherlandsFlag(arr, L, R):
     return [less + 1, more]
 
 
-def quickSort1(arr):
-    """ 快排 1.0
+def quickSort4(arr):
+    """ 快排迭代版
     """
     if not arr or len(arr) < 2:
         return
-    process1(arr, 0, len(arr) - 1)
+    N = len(arr)
+    stack = [[0, N - 1]]
+    while stack:
+        sort_area = stack.pop()
+        swap(arr, sort_area[1], random.randint(*sort_area))
+        equal_area = netherlandFlag(arr, *sort_area)
+        if equal_area[0] < equal_area[1]:
+            stack.append([sort_area[0], equal_area[0] - 1])
+            stack.append([equal_area[1] + 1, sort_area[1]])
 
-
-def process1(arr, L, R):
-    if L >= R:
-        return
-    mid = partition(arr, L, R)
-    process1(arr, L, mid - 1)
-    process1(arr, mid + 1, R)
-
-
-def quickSort2(arr):
-    if not arr or len(arr) < 2:
-        return
-    process2(arr, 0, len(arr) - 1)
-
-
-def process2(arr, L, R):
-    if L >= R:
-        return
-    equal = netherlandsFlag(arr, L, R)
-    process2(arr, L, equal[0] - 1)
-    process2(arr, equal[1] + 1, R)
 
 
 def quickSort3(arr):
@@ -97,10 +62,9 @@ def process3(arr, L, R):
         return
     num = random.randint(L, R)
     swap(arr, num, R)
-    equal = netherlandsFlag(arr, L, R)
+    equal = netherlandFlag(arr, L, R)
     process3(arr, L, equal[0] - 1)
     process3(arr, equal[1] + 1, R)
-
 
 
 # for test
@@ -120,15 +84,13 @@ if __name__ == "__main__":
     print("测试开始")
     for i in xrange(testTime):
         arr = generateRandomArray(maxSize, maxValue)
-        arr1 = arr[::]
-        arr2 = arr[::]
         arr3 = arr[::]
-        quickSort1(arr1)
-        quickSort2(arr2)
+        arr4 = arr[::]
         quickSort3(arr3)
-        if arr1 != arr2 or arr1 != arr3:
-            print(arr1)
-            print(arr2)
+        quickSort4(arr4)
+        if arr3 != arr4:
+            print(arr)
             print(arr3)
+            print(arr4)
             break
     print("测试结束")
